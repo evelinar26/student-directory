421713c5732762015523eb451582b3a79401f109
Evelina R <evelinar70@gmail.com>
2022-07-13 22:12:31 +0100

    input students method

--- directory.rb
+++ directory.rb
@@ -1,21 +1,39 @@
-students = [
-    "Dr. Hannibal Lecter",
-    "Darth Vader",
-    "Nurse Ratched",
-    "Michael Corleone",
-    "Alex DeLarge",
-    "The Wicked Witch of the West",
-    "Terminator",
-    "Freddy Krueger",
-    "The Joker",
-    "Joffrey Baratheon",
-    "Norman Bates"
-]
-# and then print them
-puts "The students of Villains Academy"
-puts "-------------"
-students.each do |student|
-    puts student
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    # add the student hash to the array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    # get another name from the user
+    name = gets.chomp
+  end
+  # return the array of students
+  students
 end
-# finally, we print the total
-puts "Overall, we have #{students.count} great students"
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+
+def print(student)
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)
+  end
+end
+
+def print_footer(students)
+  puts "Overall, we have #{names.count} great students"
+end
+
+#nothing happens until we call the methods
+students = input_students
+print_header
+print(students)
+print_footer(students)
